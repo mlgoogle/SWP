9a02df4bc63f366ae35de7397e2e9f8f0c76ffe3
kerry <kerry@miglab.com>
2016-12-31 22:39:31 +0800

    +行情插件

---
+++ plugins/quotations/quotations_logic.h
@@ -0,0 +1,50 @@
+//  Copyright (c) 2015-2015 The swp Authors. All rights reserved.
+//  Created on: 2016年12月30日 Author: kerry
+
+#ifndef SWP_QUOTATIONS_QUOTATIONS_LOGIC_
+#define SWP_QUOTATIONS_QUOTATIONS_LOGIC_
+
+#include "basic/basictypes.h"
+#include "core/common.h"
+//#include "net/comm_head.h"
+//#include "net/packet_processing.h"
+
+namespace quatations_logic {
+
+class Quotationslogic {
+public:
+  Quotationslogic();
+  virtual ~Quotationslogic();
+
+private:
+  static Quotationslogic *instance_;
+
+public:
+  static Quotationslogic *GetInstance();
+  static void FreeInstance();
+
+  bool OnQuotationsConnect(struct server *srv, const int socket);
+
+  bool OnQuotationsMessage(struct server *srv, const int socket, const void *msg,
+                       const int len);
+
+  bool OnQuotationsClose(struct server *srv, const int socket);
+
+  bool OnBroadcastConnect(struct server *srv, const int socket,
+                          const void *data, const int len);
+
+  bool OnBroadcastMessage(struct server *srv, const int socket, const void *msg,
+                          const int len);
+
+  bool OnBroadcastClose(struct server *srv, const int socket);
+
+  bool OnIniTimer(struct server *srv);
+
+  bool OnTimeout(struct server *srv, char *id, int opcode, int time);
+
+private:
+  bool Init();
+};
+} // namespace quatations_logic
+
+#endif
